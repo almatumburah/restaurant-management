37eb9bf438b49493205035b2969d2243bed8d450
Alpha <almatumburah@hotmail.com>
2022-03-04 13:35:21 +0200

    Integrate User SignUp API with Axios

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,42 +1,66 @@
 <template>
-    <img class="logo" alt="Alpha logo" src="../assets/alpha_logo.png">
-    <h1>Sign Up</h1>
-    <div class="register">
-        <input type="text" placeholder="Enter Name">
-        <input type="email" placeholder="Enter Email">
-        <input type="password" placeholder="Enter Password">
-        <input type="password" placeholder="Confirm Password">
-        <button>Sign Up</button>
-    </div>
+<img class="logo" alt="Alpha logo" src="../assets/alpha_logo.png">
+<h1>Sign Up</h1>
+<div class="register">
+    <input type="text" v-model="name" placeholder="Enter Name">
+    <input type="email" v-model="email" placeholder="Enter Email">
+    <input type="password" v-model="password" placeholder="Enter Password">
+    <button v-on:click="signUp" >Sign Up</button>
+</div>
 </template>
 <script>
-    export default {
-        name :'SignUp'
-    }
+import axios from 'axios'
+export default {
+    name :'SignUp',
+    data() {
+        return {
+            name:'',
+            email:'',
+            password:''
+        }
+    },
+    methods: {
+        async signUp()
+        {
+            let result = await axios.post("http://localhost:3000/users", {
+                name:this.name,
+                email:this.email,
+                password:this.password
+            });
+
+            console.log(result);
+            if(result.status==201)
+            {
+                alert("sign-up successful");
+                localStorage.setItem("user-reg", JSON.stringify(result.data));
+            }
+        }
+    },
+}
 </script>
 
 <style>
-    .logo{
-        width: 100px;
-    }
-    .register input{
-    width: 300px;
-    height: 40px;
-    padding-left: 20px;
-    display: block;
-    margin-bottom: 30px;
-    margin-right: auto;
-    margin-left: auto;
-    border: 2px solid black;
-    border-radius: 5px;
-    }
-    .register button{
-    cursor: pointer;
-    width: 320px;
-    height: 45px;
-    color: white;
-    border: 2px solid black;
-    background: black;
-    border-radius: 5px;
-    }
+.logo{
+    width: 100px;
+}
+.register input{
+width: 300px;
+height: 40px;
+padding-left: 20px;
+display: block;
+margin-bottom: 30px;
+margin-right: auto;
+margin-left: auto;
+border: 2px solid black;
+border-radius: 5px;
+}
+.register button{
+cursor: pointer;
+width: 320px;
+height: 45px;
+color: white;
+border: 2px solid black;
+background: black;
+border-radius: 5px;
+}
 </style>
